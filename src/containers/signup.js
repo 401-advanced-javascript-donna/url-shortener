779c212802934612687fba9f4f87ed7f7be730d5
sanvean74 <dlambertpdx@gmail.com>
2019-12-02 11:48:37 -0800

    compile err fixed

--- src/containers/signup.js
+++ src/containers/signup.js
@@ -1,11 +1,19 @@
 import { connect } from 'react-redux';
+import UserForm from '../components/user/UserForm';
+import { sessionSignup } from '../actions/sessionActions';
 
 const mapStateToProps = () => ({
-  buttonText: 'SignUp'
+  buttonText: 'Signup'
 });
 
-const mapDispathToProps = () => ({
+const mapDispatchToProps = dispatch => ({
   handleSubmit(event, username, password) {
     event.preventDefault();
+    dispatch(sessionSignup(username, password));
   }
-})
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(UserForm);
